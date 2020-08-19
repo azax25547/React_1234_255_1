2129a108ee922efa3dd38793c18d7a286b74fbc8
azax25547 <momentumangular@gmail.com>
2020-08-19 06:12:13 +0530

    Listed Todos

--- src/App.js
+++ src/App.js
@@ -1,10 +1,45 @@
 import React from "react";
+import { useQuery } from "@apollo/react-hooks";
+import { gql } from "apollo-boost";
+
+const GET_TODOS = gql`
+  query MyQuery {
+    todo {
+      done
+      id
+      text
+    }
+  }
+`;
 
 function App() {
+  const { data, loading, error } = useQuery(GET_TODOS);
+  if (loading) return <div>Loading....</div>;
+  if (error) return <div>Error</div>;
   return (
-    <>
-      <h1>Welcome to GraphQL Todos</h1>
-    </>
+    <div className="vh-100 code flex flex-column items-center bg-purple white pa3 fl-1">
+      <h1 className="fl-1">Welcome to GraphQL Todos</h1>
+      <form className="mb3">
+        <input
+          className="pa2 f4 b--dashed"
+          type="text"
+          placeholder="What to do "
+        />
+        <button type="submit" className="pa2 f4 bg-green">
+          Submit
+        </button>
+      </form>
+      <div className="flex items-center justify-center flex-column">
+        {data.todo.map((todo) => (
+          <p key={todo.id}>
+            <span className="pointer list pa1 f3 ">{todo.text}</span>
+            <span className="bg-transparent bn f4">
+              <span className="red">&times;</span>
+            </span>
+          </p>
+        ))}
+      </div>
+    </div>
   );
 }
 
--- src/index.js
+++ src/index.js
@@ -1,6 +1,19 @@
 import React from "react";
 import ReactDOM from "react-dom";
 
+import ApolloClient from "apollo-boost";
+import { ApolloProvider } from "@apollo/react-hooks";
 import App from "./App";
 
-ReactDOM.render(<App />, document.getElementById("root"));
+// https://stable-frog-55.hasura.app/console/api-explorer
+
+const client = new ApolloClient({
+  uri: "https://stable-frog-55.hasura.app/v1/graphql",
+});
+
+ReactDOM.render(
+  <ApolloProvider client={client}>
+    <App />
+  </ApolloProvider>,
+  document.getElementById("root")
+);
